87211bbf12c13aa4cddd580f03f2a7ee3316d870
Patrik Pralas <patrik.pralas@q.agency>
2023-04-20 13:29:52 +0200

    Added form page with post method

---
+++ lib/domain/notifiers/post_state_notifier.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:new_weather/data/model/post_repository.dart';
+import 'package:new_weather/domain/entities/post.dart';
+import 'package:new_weather/domain/notifiers/post_state.dart';
+import 'package:riverpod/riverpod.dart';
+
+final postValueProvider = StateNotifierProvider<PostStateNotifier, PostState>(
+    (ref) =>
+        PostStateNotifier(ref.watch(postRepositoryProvider))..publishPost());
+
+class PostStateNotifier extends StateNotifier<PostState> {
+  final PostRepository _postRepository;
+
+  PostStateNotifier(this._postRepository)
+      : super(
+          const PostState.empty(),
+        );
+
+  void publishPost({Post? post}) async {
+    final result = await _postRepository.createPost(post!);
+    result.fold(
+        // state u svakoj od linija ispod updatea state
+        (error) => state = PostState.failure(error),
+        (post) => state = PostState.posted(post));
+  }
+}
